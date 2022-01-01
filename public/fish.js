cdd3ebdf19fb9a6e0f4100c4fd900631fa1ff36b
YH-HO1997 <85390604+YH-HO1997@users.noreply.github.com>
2022-01-01 23:07:18 +0800

    Add files via upload

--- public/fish.js
+++ public/fish.js
@@ -2,29 +2,3 @@ let point = 0;
 document.getElementById('point').textContent = point;
 let day = 30;
 document.getElementById('day').textContent = day;
-
-let wrapper=document.getElementById('wrapper');
-let boat;
-
-function preload() {
-    boat = createSprite(100, 200);
-    boat.addImage('boat', loadImage('images/plane.jpg'));
-
-}
-
-function setup() {
-    
-    createCanvas(wrapper.clientWidth, 600);
-}
-
-function draw() {
-    
-    if (keyIsDown(LEFT_ARROW)){ // left
-        boat.setVelocity(-3, 0);
-    } else if (keyIsDown(RIGHT_ARROW)){
-        boat.setVelocity(3, 0);
-    } else { // no key press ‐> stand still
-        boat.setVelocity(0, 0); 
-    }
-    drawSprites();
-}
