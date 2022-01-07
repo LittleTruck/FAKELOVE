eaa8bd89a12d14abde183713b02edf57d646b7e9
LittleTruck <i839855314i@gmail.com>
2022-01-07 10:14:33 +0800

    setup code adjust、fish swimming loop

--- public/fish.js
+++ public/fish.js
@@ -1,9 +1,7 @@
 let point = 0;
-document.getElementById('point').textContent = point;
 let day = 30;
-document.getElementById('day').textContent = day;
 
-let wrapper = document.getElementById('wrapper');
+let wrapper;
 let boat;
 let love;
 let money;
@@ -16,21 +14,15 @@ function preload() {
     boat.addImage('boat', loadImage('images/boat.png'));
     boat.scale = 0.2;
     Bait = createSprite(230, 160);
-    Bait.shapeColor= '#ffffff00';
+    Bait.shapeColor = '#ffffff00';
 
     love = loadImage('images/chooseBait/love_hover.png');
     money = loadImage('images/chooseBait/money_hover.png');
     sex = loadImage('images/chooseBait/sex_hover.png');
 
-    for (var i = 1; i < 39; i++) {
+    for (let i = 1; i < 39; i++) {
         // x: 100~2800, y: 250~1200
         fishes[i] = createSprite(getRandom(100, 2800), getRandom(250, 1200));
-        console.log(Math.random() >= 0.5 ? 1 : -1);
-        const dir = Math.random() >= 0.5 ? 1 : -1;
-        fishes[i].mirrorX(dir);
-
-        var velocity = dir == 1 ? getRandom(-1, -3) : getRandom(1, 3);
-        fishes[i].setVelocity(velocity, 0);
 
         fishes[i].scale = 0.2;
         fishes[i].addImage(loadImage('images/fish/' + i + '.png'));
@@ -39,7 +31,18 @@ function preload() {
 }
 
 function setup() {
+    wrapper = document.getElementById('wrapper');
     createCanvas(wrapper.clientWidth, wrapper.clientHeight);
+    document.getElementById('point').textContent = point;
+    document.getElementById('day').textContent = day;
+
+    for (let i = 1; i < 39; i++) {
+        const dir = Math.random() >= 0.5 ? 1 : -1;
+        fishes[i].mirrorX(dir);
+
+        const velocity = dir == 1 ? getRandom(-1, -3) : getRandom(1, 3);
+        fishes[i].setVelocity(velocity, 0);
+    }
 }
 
 function draw() {
@@ -73,16 +76,23 @@ function draw() {
         Bait.addImage(love);
         Bait.scale = 0.08;
         bait_move();
-    }else if (currentBait == "money"){
+    } else if (currentBait == "money") {
         Bait.addImage(money);
         Bait.scale = 0.08;
         bait_move();
-    }else if (currentBait == "sex"){
+    } else if (currentBait == "sex") {
         Bait.addImage(sex);
         Bait.scale = 0.08;
         bait_move();
     }
-
+    
+    for (let i = 1; i < fishes.length; i++) {
+        if (fishes[i].position.x < -100)
+            fishes[i].position.x = 3000;
+        else if (fishes[i].position.x > 3000)
+            fishes[i].position.x = -100;
+    }
+    
     drawSprites();
 }
 
@@ -90,25 +100,25 @@ function draw() {
 function bait_move() {
     if (keyIsDown(LEFT_ARROW)) { // left
         //Bait.setVelocity(-3, 0);
-        Bait.position.x=boat.position.x+130;
-        Bait.position.y=boat.position.y+40;
+        Bait.position.x = boat.position.x + 130;
+        Bait.position.y = boat.position.y + 40;
 
     } else if (keyIsDown(RIGHT_ARROW)) {
         //Bait.setVelocity(3, 0);
-        Bait.position.x=boat.position.x+130;
-        Bait.position.y=boat.position.y+40;
-        
+        Bait.position.x = boat.position.x + 130;
+        Bait.position.y = boat.position.y + 40;
+
     } else if (keyIsDown(DOWN_ARROW)) {
-        Bait.position.y=Bait.position.y+20;
+        Bait.position.y = Bait.position.y + 20;
         stroke(255);
         strokeWeight(3);
         line(Bait.position.x - 20, boat.position.y - 80, Bait.position.x, Bait.position.y);
     } else if (keyIsDown(UP_ARROW)) {
-        Bait.position.y=Bait.position.y-20;
+        Bait.position.y = Bait.position.y - 20;
         stroke(255);
         strokeWeight(3);
         line(Bait.position.x - 20, boat.position.y - 80, Bait.position.x, Bait.position.y);
-    }else { // no key press ‐> stand still
+    } else { // no key press ‐> stand still
         Bait.setVelocity(0, 0);
     }
 }
@@ -135,6 +145,7 @@ function setBaitToLove() {
 function setBaitToMoney() {
     currentBait = "money";
 }
+
 function setBaitToSex() {
     currentBait = "sex";
 }
